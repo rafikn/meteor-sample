f30094066b156062eb95fbcc5edab8cc2d2acfff
Rafik Nasraoui <rafik.nasraoui@gmail.com>
2016-03-23 12:38:57 +1300

    Added npm packages utils

--- client/jsx/client.jsx
+++ client/jsx/client.jsx
@@ -1,6 +1,4 @@
-if (Meteor.isClient) {
-  Meteor.startup(function() {
-    const app = document.getElementById('app');
-    ReactDOM.render(<Layout/>, app);
-  });
-}
+Meteor.startup(function() {
+  const app = document.getElementById('app');
+  ReactDOM.render(<Layout/>, app);
+});
